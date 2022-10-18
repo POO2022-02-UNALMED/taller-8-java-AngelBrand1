43e25c417d02a6aec182062cde78fdb753b035c5
Angel Brand <angeldavidbran000@gmail.com>
2022-10-18 09:56:16 -0500

    intento3

---
+++ src/test/java/futbol/Futbolista.java
@@ -0,0 +1,60 @@
+package futbol;
+public abstract class Futbolista implements Comparable<Futbolista> {
+	private String nombre;
+	private int edad;
+	private String posicion;
+	//constructor
+	public Futbolista(String nombre, int edad, String posicion) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.posicion = posicion;
+	}
+	
+	public Futbolista() {
+		this("maradona",30,"delantero");
+	}
+	
+	//metodos
+	public boolean equals(Futbolista f) {
+		return(this==f);
+	}
+	
+	public String toString() {
+		String s = 
+				String.format(
+				"El futbolista %s tiene %d, y juega de %s"
+				,this.nombre,this.edad,this.posicion);
+				
+		return s;
+	}
+	
+	public abstract boolean jugarConLasManos();
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+	
+	
+}
+
---
+++ src/test/java/futbol/Jugador.java
@@ -0,0 +1,61 @@
+package futbol;
+import static java.lang.Math.abs; 
+
+public class Jugador extends Futbolista {
+	private short golesMarcados;
+	private byte dorsal;
+
+	//constructores
+	public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal) {
+		super(nombre, edad, posicion);
+		this.golesMarcados = golesMarcados;
+		this.dorsal = dorsal;
+	}
+	
+	public Jugador() {
+		super();
+		this.golesMarcados = 289;
+		this.dorsal = 7;
+	}
+	//metodos
+	public boolean jugarConLasManos() {
+		return false;
+	}
+	
+	@Override
+	public int compareTo(Futbolista o) {
+		Jugador n = (Jugador)o;
+		int goles = n.getGolesMarcados() ;
+		
+		return abs(this.golesMarcados-goles);
+	}
+	
+	public String toSring() {
+		String s = 
+				String.format(
+						"El futbolista %s tiene %d, y juega de %s con el dorsal %d. Ha marcado %d"
+						, this.getNombre(),this.getEdad(),this.getPosicion(),this.dorsal,this.golesMarcados);
+		return s;
+	}
+	//getters and setters
+
+	public short getGolesMarcados() {
+		return golesMarcados;
+	}
+
+	public void setGolesMarcados(short golesMarcados) {
+		this.golesMarcados = golesMarcados;
+	}
+
+	public byte getDorsal() {
+		return dorsal;
+	}
+
+	public void setDorsal(byte dorsal) {
+		this.dorsal = dorsal;
+	}
+
+
+	
+	
+}
